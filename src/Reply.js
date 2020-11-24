9598a19623e5ecf8333392f7fc27b714bab34382
Sean Grove <sean@bushi.do>
2020-11-24 00:13:47 -0800

    Updates from OneGraph code generator

--- src/Reply.js
+++ src/Reply.js
@@ -50,7 +50,7 @@ export default function ReplyMutation(props) {
 
   const dataEl = mutationResult?.data ? (
     <div className="data-box">
-      <h3>Data for ReplyMutation <LocationNote location={'Reply.js:<Reply>'} /></h3>
+      <h3>Data for ReplyMutation</h3>
       <pre>{stringifyRelayData(mutationResult.data)}</pre>
       <h4>CommentUses</h4>
       {commentUses}
@@ -94,7 +94,7 @@ export default function ReplyMutation(props) {
 
   return (
     <div>
-      <h3>ReplyMutation</h3>
+      <h3>ReplyMutation <LocationNote location={'Reply.js:<Reply>'} /></h3>
       {formEl}
       {actionButton}
       {loadingEl}
